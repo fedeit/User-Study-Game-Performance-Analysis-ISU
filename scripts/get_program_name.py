8ec61430faadf7f8bdf53e6ddb581be38992fc39
lab-setup <91223290+fedeit2@users.noreply.github.com>
2022-02-22 15:40:28 -0500

    Utility update

--- scripts/get_program_name.py
+++ scripts/get_program_name.py
@@ -1,7 +1,22 @@
 import win32gui
+import win32con
+import win32api
+import win32process as wproc
 
 def winEnumHandler( hwnd, ctx ):
     if win32gui.IsWindowVisible( hwnd ):
         print (hex(hwnd), win32gui.GetWindowText( hwnd ))
 
 win32gui.EnumWindows( winEnumHandler, None )
+
+def maximizeWindows(name): 
+    try:
+        whnd = win32gui.FindWindow(None, name)
+        win32gui.ShowWindow(whnd, win32con.SW_MAXIMIZE)
+        remote_thread, _ = wproc.GetWindowThreadProcessId(whnd)
+        wproc.AttachThreadInput(win32api.GetCurrentThreadId(), remote_thread, True)
+        win32gui.SetFocus(whnd)
+    except:
+        return
+
+maximizeWindows('Rainbow Six')
