8f0abca813e64febcea103a5bf51b0b58424b6d5
fedeit <fgalbiati@wpi.edu>
2022-03-29 04:23:48 -0400

    Pipeline for data preprocessing

---
+++ data-processing/process_testset.py
@@ -0,0 +1,15 @@
+from concurrent.futures import process
+from glob import glob
+import pandas as pd
+import process_trial
+import json
+for test_group in glob('data/raw/*/metrics_*.csv'):
+    print(test_group)
+    platform = test_group.split('/')[2]
+    df = pd.read_csv(test_group)
+    df['init_time'] = pd.to_datetime(df['init_time'])  
+    df.reset_index(inplace=True)
+    for index, row in df.iterrows():
+        row['times'] = json.loads(row['times'])
+        hw_log = test_group.replace('metrics_', 'hardware_log_')
+        process_trial.compute(row, hw_log, index, platform)
